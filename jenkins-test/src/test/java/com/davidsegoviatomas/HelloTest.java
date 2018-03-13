b6d77d205706abd3adb73b5637b68f9cb529b862
David <david17sego@gmail.com>
2018-03-13 17:32:17 +0100

    another try

--- jenkins-test/src/test/java/com/davidsegoviatomas/HelloTest.java
+++ jenkins-test/src/test/java/com/davidsegoviatomas/HelloTest.java
@@ -13,7 +13,7 @@ public class HelloTest {
     @Test
     public void helloTest() throws Exception {
         // im just a comment
-        assertEquals(true, false);
+        assertEquals(true, true);
     }
 
 }
